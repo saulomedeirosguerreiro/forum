838115840e0c3ac1868d79feaaff2ad129bcb7aa
sauloguerreiro <s.guerreiro@swordhealth.com>
2023-06-14 09:48:43 +0100

    feature: add new post

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,6 +3,23 @@
         <h1>{{ thread.title }}</h1>
   
         <post-list :posts="threadPosts"/>
+
+        <form @submit.prevent="addPost">
+              <div class="form-group">
+                <label for="thread_title">Title:</label>
+                <input type="text" id="thread_title" class="form-input" name="title" >
+              </div>
+
+              <div class="form-group">
+                <label for="thread_content">Content:</label>
+                <textarea v-model="newPostText" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
+              </div>
+
+              <div class="btn-group">
+                <button class="btn btn-ghost">Cancel</button>
+                <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
+              </div>
+          </form>
     </div>
   </template>
   
@@ -13,6 +30,7 @@
   
   const posts = reactive(sourceData.posts);
   const threads = reactive(sourceData.threads);
+  let newPostText = reactive('');
 
   defineComponent({
     components: {
@@ -29,6 +47,20 @@
 
   const thread = computed(() => threads.find(thread => thread.id === props.id));
   const threadPosts = computed(() => posts.filter(post => post.threadId === props.id));
+
+  function addPost() {
+    const postId = 'gggg' + Math.random();
+    const post = {
+      id: postId,
+      text: this.newPostText,
+      publishedAt: Math.floor(Date.now() / 1000),
+      threadId: this.id,
+      userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+    }
+    posts.push(post);
+    threads.posts.push(post);
+    newPostText = '';
+  }
   </script>
   
   <style>
