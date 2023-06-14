a199a70bb4b4877ff82c6307a256feabe332b6db
sauloguerreiro <s.guerreiro@swordhealth.com>
2023-06-14 08:47:04 +0100

    chore: organize project structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import PageHome from '@/components/PageHome';
-import PageThreadShow from '@/components/PageThreadShow';
+import Home from '@/components/Home';
+import ThreadShow from '@/components/ThreadShow';
 import NotFound from '@/components/NotFound';
 import { createRouter, createWebHistory} from 'vue-router';
 import sourceData  from '@/data.json';
@@ -8,12 +8,12 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: PageHome,
+        component: Home,
     },
     {
         path: '/thread/:id',
         name: 'ThreadShow',
-        component: PageThreadShow,
+        component: ThreadShow,
         props: true,
         beforeEnter(to, from, next) {
             const threadExists = sourceData.threads.find(thread => thread.id === to.params.id);
