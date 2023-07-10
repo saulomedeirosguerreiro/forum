8fa1f982d73bd154f83ad4e714ced853ecb39fff
sauloguerreiro <s.guerreiro@swordhealth.com>
2023-07-10 09:44:56 +0100

    chore: configure firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyD6h2etFVhqEjALKqw6PiQXVdFoKk7k3F8",
+  authDomain: "forum-app-d8da8.firebaseapp.com",
+  projectId: "forum-app-d8da8",
+  storageBucket: "forum-app-d8da8.appspot.com",
+  messagingSenderId: "990028131702",
+  appId: "1:990028131702:web:d284cb6802fc1c01e0de57"
+};
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from '@/App'
 import router from '@/router'
 import AppDate from '@/components/AppDate.vue';
 import store from '@/store';
+import firebase from 'firebase';
+import firebaseConfig from '@/config/firebase'
+
+firebase.initializeApp(firebaseConfig);
 
 const forumApp = createApp(App);
 forumApp.use(router);
